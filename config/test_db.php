7fbd0f79ca936dc016db46843b72f5e9a2243d38
Ivan Sidorov <nobxod@gmail.com>
2020-09-21 20:28:09 +0300

    Configuration DB for testing via Docker

--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,8 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2basic_test';
+$db['dsn'] = 'mysql:host=localhost;dbname=project';
+$db['username'] = 'project';
+$db['password'] = 'project';
 
 return $db;
